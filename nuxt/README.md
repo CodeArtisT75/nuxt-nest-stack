cb4b1f792aa9e61fdaf1d871dc6ed7bdf3478379
CodeArtisT75 <pcstar7000@gmail.com>
2020-08-25 15:56:57 +0430

    fix content

--- nuxt/README.md
+++ nuxt/README.md
@@ -1,5 +1,3 @@
-# sokanacademy_nuxt
-
 ## Build Setup
 
 ```bash
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <Logo />
       <h1 class="title">
-        sokanacademy_nuxt
+        Nest Nuxt
       </h1>
       <div class="links">
         <a
